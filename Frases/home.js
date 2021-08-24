bae842d2c8bfce4f445ea05fe6f977d603d95d14
Hugo Corrêa Rosa <37915928+hugocrosa@users.noreply.github.com>
2021-08-24 12:00:35 -0300

    Add files via upload
    Ajuste na exibição da frase

---
+++ Frases/home.js
@@ -0,0 +1,27 @@
+main()
+function frase(url) {
+   let request = new XMLHttpRequest();
+   request.open("GET", url, false);
+   request.send();
+   let advice = request.responseText;
+   let json = JSON.parse(advice);
+   return json.slip.advice;
+ 
+    }
+    
+    function main(){
+       document.body.innerHTML= (frase("https://api.adviceslip.com/advice"));
+
+       document.body.style.fontSize = '24px';
+       document.body.style.color = 'green';
+       document.body.style.fontFamily = 'arial';
+       document.body.style.border = "thick solid #008000";
+    }
+
+
+
+
+    
+
+
+
